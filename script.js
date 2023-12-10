f19bc81b744e07cd089610697cf45ed09cffc545
shivakarka <shivakarka@gmail.com>
2023-12-11 02:35:06 +0530

    Fetch footer data from api

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const productSection = document.querySelector(".product-section");
+const footerDiv = document.querySelector(".footer");
 
 const initStore = () => {
   loadDataFromUrl("https://sandbox.nextleap.app/products/fetch").then(
     (products) => {
       renderProducts(products);
+      renderFooter(products);
     }
   );
 };
@@ -60,14 +62,13 @@ const renderProducts = (products) => {
               } &starf;</span> ${product.ratings.totalRatingsNum.toLocaleString()} Ratings &
               ${product.ratings.totalReviewsNum.toLocaleString()} Reviews
             </p>
-
             <ul>
-              <li>${product.featuresList[0]}</li>
-              <li>${product.featuresList[1]}</li>
-              <li>${product.featuresList[2]}</li>
-              <li>${product.featuresList[3]}</li>
-              <li>${product.featuresList[4]}</li>
-            </ul>
+              ${product.featuresList
+                .map((feature) => {
+                  return `<li>${feature}</li>`;
+                })
+                .join("")}
+             </ul>
           </div>
         </div>
         <div class="product-right">
@@ -82,6 +83,75 @@ const renderProducts = (products) => {
       </div>
               `;
   });
+};
+
+const renderFooter = (products) => {
+  const footerItem1 = products?.footer.colunms;
+  const footerItem2 = products?.footer.bottomColunms;
+
+  const footerRightData1 = footerItem1[4].data.split(",");
+  const footerRightData2 = footerItem1[5].data.split(",");
+
+  footerDiv.innerHTML += `
+     <div class="footer-top">
+          <div class="footer-left">
+      ${footerItem1
+        .slice(0, 4)
+        .map(
+          (item) => `
+          <div>
+            <h6 class="about">${item.title}</h6>
+            <ul>
+              ${item.data.map((data) => `<li>${data}</li>`).join("")}
+            </ul>
+          </div>
+        `
+        )
+        .join("")}
+    </div>
+        <div class="footer-right">
+          <div>
+            <h6>${footerItem1[4].title}</h6>
+            <p>${footerRightData1[0]}</p>
+            <p>${footerRightData1[1]}, ${footerRightData1[2].slice(0, 10)}</p>
+            <p>${footerRightData1[2].slice(10)},</p>
+            <p>${footerRightData1[3]}, ${footerRightData1[4]},</p>
+            <p>${footerRightData1[5]}, ${footerRightData1[6]},</p>
+            <p>${footerRightData1[7]}, ${footerRightData1[8]}</p>
+          </div>
+          <div>
+            <h6>${footerItem1[5].title}</h6>
+            <p>${footerRightData2[0]},</p>
+            <p>${footerRightData2[1]}, ${footerRightData2[2].slice(0, 10)}</p>
+            <p>${footerRightData2[2].slice(10)},</p>
+            <p>${footerRightData2[3]}, ${footerRightData2[4]},</p>
+            <p>${footerRightData2[5]}, ${footerRightData2[6]},</p>
+            <p>${footerRightData1[7]}, ${footerRightData1[8]}</p>
+            <p>${footerRightData2[8].slice(6, 34)}</p>
+            <p>${footerRightData2[8].slice(35)}</p>
+          </div>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        ${footerItem2
+          .map((item) => {
+            return `<div>
+           ${
+             item.img
+               ? `<img
+            src=${item.img}
+            alt="logo"
+          />`
+               : ""
+           }
+          ${item.text ? `<a href="">${item?.text}</a>` : ""}
+        </div>
+        `;
+          })
+          .join("")} 
+   
+      </div>
+      `;
 };
 
 document.addEventListener("DOMContentLoaded", initStore);
